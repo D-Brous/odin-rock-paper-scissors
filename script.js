190f59162168c75c4630dd191a68cbdfe765d3ba
Daniel Brous <90479372+D-Brous@users.noreply.github.com>
2026-09-24 23:30:05 -0400

    Add game logic

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
 let computerScore, humanScore = 0;
 
 function getComputerChoice() {
-    const randomNumber = Math.random();
-    if (randomNumber < 1/3) {
-        return "rock";
-    } else if (randomNumber < 2/3) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+  const randomNumber = Math.random();
+  if (randomNumber < 1/3) {
+    return "rock";
+  } else if (randomNumber < 2/3) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
 function getHumanChoice() {
-    return window.prompt("Enter \"rock\", \"paper\", or \"scissors\": ");
+  return window.prompt("Enter \"rock\", \"paper\", or \"scissors\": ");
 }
+
+function playRound(computerChoice, humanChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  let outcome = 0;
+  if (computerChoice == humanChoice) {
+    outcome = 1;
+  } else if (
+    (computerChoice == "rock" && humanChoice == "paper") ||
+    (computerChoice == "paper" && humanChoice == "scissors") ||
+    (computerChoice == "scissors" && humanChoice == "rock")
+  ) {
+    outcome = 2;
+  }
+  
+  if (outcome == 0) {
+    computerScore += 1;
+    console.log("You lost... * womp womp *");
+  } else if (outcome == 2) {
+    humanScore += 1;
+    console.log("You won! Hooray!");
+  } else {
+    console.log("You tied. -_-");
+  }
+}
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+playRound(computerSelection, humanSelection);
